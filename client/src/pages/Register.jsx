3c2b5cd6ca874858656d9c0136140787edca75ae
nathandev254 <warimwenathaniel@gmail.com>
2023-07-07 16:05:19 +0300

    Todays work happy weekend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import {yupResolver} from '@hookform/resolvers/yup'
 
 function Register() {
+  const schema = yup.object().shape({
+    username: yup.string().required(),
+    firstname: yup.string().required(),
+    lastname: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(5).max(15).required(),
+    confirmpassword: yup
+      .string()
+      .oneOf([yup.ref("password"),null])
+      .required(),
+  });
+
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="Register--container">
       <div className="Register--left">
@@ -17,15 +40,37 @@ function Register() {
         </div>
       </div>
       <div className="Register--right">
-        <form action="" className="form--Register">
+        <form
+          action=""
+          className="form--Register"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <h1>Create Account</h1>
-          <input type="text" placeholder="username" />
-          <input type="text" placeholder="firstname" />
-          <input type="text" placeholder="lastname" />
-          <input type="email" placeholder="email" />
-          <input type="password" name="" id="" placeholder="password" />
-          <input type="password" name="" id="" placeholder="ConfirmPassword" />
-          <button className="Register--button">sign up</button>
+          <input type="text" placeholder="username" {...register("username")} />
+          <input
+            type="text"
+            placeholder="firstname"
+            {...register("firstname")}
+          />
+          <input type="text" placeholder="lastname" {...register("lastname")} />
+          <input type="email" placeholder="email" {...register("email")} />
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="password"
+            {...register("password")}
+          />
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="ConfirmPassword"
+            {...register("confirmpassword")}
+          />
+          <button className="Register--button" type="submit">
+            sign up
+          </button>
         </form>
       </div>
     </div>
