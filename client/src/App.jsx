adf7e2f32e54c2478e2fc6969a019d92e66a1400
nathandev254 <warimwenathaniel@gmail.com>
2023-07-07 15:26:42 +0300

    Todays work happy weekend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import Register from "./pages/Register";
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="register" element={<Register />}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -8,11 +8,25 @@ function Register() {
       <div className="Register--left">
         <div className="Register--content">
           <h1 className="Register--heading">Welcome Back!</h1>
-          <p className="Register--paragraph">To keep connected with us please login with your personal info</p>
-          <Link className="Login--button" to="register">
+          <p className="Register--paragraph">
+            To keep connected with us please login with your personal info
+          </p>
+          <Link className="Login--button" to="login">
             sign in
           </Link>
         </div>
+      </div>
+      <div className="Register--right">
+        <form action="" className="form--Register">
+          <h1>Create Account</h1>
+          <input type="text" placeholder="username" />
+          <input type="text" placeholder="firstname" />
+          <input type="text" placeholder="lastname" />
+          <input type="email" placeholder="email" />
+          <input type="password" name="" id="" placeholder="password" />
+          <input type="password" name="" id="" placeholder="ConfirmPassword" />
+          <button className="Register--button">sign up</button>
+        </form>
       </div>
     </div>
   );
