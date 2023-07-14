8679d9f1cdfd8857a31286537340b1a43bbaf798
nathandev254 <warimwenathaniel@gmail.com>
2023-07-14 16:42:39 +0300

    niice

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
