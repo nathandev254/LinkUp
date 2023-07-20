f5794be7b6a6fda1738b4708c52332516ec4d9fd
nathandev254 <warimwenathaniel@gmail.com>
2023-07-20 16:28:34 +0300

    Happy weekend

--- backend/controllers/CommentController.js
+++ backend/controllers/CommentController.js
@@ -3,27 +3,51 @@ import Config from "../model/Configuration.js";
 
 export const CreateComment = async (req, res) => {
   const { post_id } = req.params;
-  const { description,user_id } = req.body;
+  const { description, user_id } = req.body;
+  // console.log(post_id,description,user_id)
   try {
     const pool = await sql.connect(Config);
     await pool
       .request()
       .input("user_id", sql.Int, user_id)
       .input("post_id", sql.Int, post_id)
-      .input("description", sql.Int, description)
+      .input("description", sql.VarChar, description)
       .query(
         "INSERT INTO comments (user_id,post_id,description) VALUES (@user_id,@post_id,@description) "
       );
-      res.json({ message: "Comment created successfully" });
+    res.json({ message: "Comment created successfully" });
   } catch (error) {
-    res.status(400).send(error)
+    // res.status(400).send(error);
+    res.send(error);
   }
-  
 };
 
+// Assuming you have imported the required libraries and configurations
+
 export const GetComments = async (req, res) => {
-  
-  res.json({ message: "Comment Accessed successfully" });
+  const { post_id } = req.params;
+  try {
+    const pool = await sql.connect(Config);
+    const results = await pool
+      .request()
+      .input("post_id", sql.Int, post_id)
+      .query(
+        "SELECT comments.comment_id, comments.description " +
+          "FROM comments " +
+          "JOIN posts ON comments.post_id = posts.post_id " +
+          "WHERE posts.post_id = @post_id"
+      );
+
+    const comments = results.recordset; 
+    res.send(results)
+    // res
+    //   .status(200)
+    //   .json({ message: "Comments accessed successfully", comments });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving comments" });
+  }
 };
 
 export const DeleteComment = async (req, res) => {
--- backend/routes/CommentRoute.js
+++ backend/routes/CommentRoute.js
@@ -1,15 +1,12 @@
 import express from "express";
 import {
   CreateComment,
-  DeleteComment,
-  UpdateComment,
   GetComments,
 } from "../controllers/CommentController.js";
 
 const CommentRouter = express.Router();
 
-CommentRouter.route("/comment").get(GetComments);
-CommentRouter.route("/comment/:post_id").post(CreateComment);
+CommentRouter.route("/comment/:post_id").post(CreateComment).get(GetComments);
 
 
 export default CommentRouter;
